99d204f832d239739d38a96abefd42bcf1b3f0f9
Gibbon <44937323+atsb@users.noreply.github.com>
2021-04-08 13:07:07 +0200

    Initial upload

---
+++ crt.h
@@ -0,0 +1,21 @@
+//
+//  crt.h
+//  Chocolate Wolfenstein 3D
+//
+//  Created by fabien sanglard on 2014-08-26.
+//
+//
+#ifndef Chocolate_Wolfenstein_3D_crt_h
+#define Chocolate_Wolfenstein_3D_crt_h
+
+#include <SDL2/SDL.h>
+
+extern SDL_Color curpal[256];
+
+/*
+ *   Trigger the Digital To Analogic convertion
+ */
+void CRT_DAC(void);
+
+void CRT_Screenshot(void);
+#endif
